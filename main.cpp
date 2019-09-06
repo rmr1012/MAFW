1ecb384825076a32b9f19de68182b9ff6703f1b7
Dennis Ren <rmr1012@gmail.com>
2019-09-06 00:38:03 -0700

    adde oneshot test feature

--- main.cpp
+++ main.cpp
@@ -10,7 +10,11 @@
 DigitalOut led1(LED1);
 DigitalOut led3(LED3);
 
-
+DigitalOut pulseO(PC_8);
+Timeout TOOneshot;
+void pulseLow(){
+  pulseO.write(0);
+}
 
 // InterruptIn armSwitch(PB_10);
 // InterruptIn loadMeter(PC_15);
@@ -28,6 +32,9 @@ std::string Inarticulate::buffer; // initialize buffer for all inarticulate clas
 int tik,tok;
 void parseCommand(string);
 void parseStage(string inStr);
+void parseOneshot(string inStr);
+
+void pollADC();
 Machine MA;
 Meter meter0(&theTerm,0, PC_14, PC_13);
 Stage stage0(&theTerm,0 , &meter0, 0.1 , PA_0 , PA_1 , PC_10 );
@@ -45,6 +52,7 @@ void flip(){
 
 int main()
 {
+    pulseO.write(1);
     initStats();
     #ifdef TERMDEBUG
     theTerm.setDebug(&debugPort);
@@ -65,6 +73,9 @@ int main()
     articulator.attach(articulate,0.1);
     statsTicker.attach(updateCPUStats,0.5);
     queue->dispatch_forever();
+
+    pulseO.write(0);
+
     while (true) { // polling loop yoooo
         wait(.2); // static delay
         led1=!led1; // life LED
@@ -86,6 +97,10 @@ void parseCommand(string cmd){
   }
   else if(strInStr("STAGE ",cmd)){ // print all stages
     parseStage(cmd);
+  }
+  else if(strInStr("ONESHOT ",cmd)){ // print oneshot
+    printf("Setting up oneshot\n");
+    parseOneshot(cmd);
   }
   else if (strInStr("STATE",cmd)){
     theTerm.printf(MA.report().c_str());
@@ -121,6 +136,9 @@ void parseCommand(string cmd){
     theTerm.printf("idle cmd\n");
     MA.idle();
   }
+  else{
+    printf("Command not found\n");
+  }
 }
 
 void parseStage(string inStr){
@@ -140,6 +158,22 @@ void parseStage(string inStr){
       else{
         stage0.setWidth(val);
       }
+  }
+  else{
+    printf("Syntax error, check syntax or type: help\n");
+  }
+}
+
+void parseOneshot(string inStr){
+  // theTerm.printDebug((char*)inStr.c_str());
+  regex IORre("ONESHOT ([0-9]*)");// pulse width(us)
+  smatch match;
+  if(regex_search(inStr,match,IORre)){ // check syntax
+      // try {
+      // theTerm.printDebug("IO read syntax matched!\n");
+      int pulseWidth=stoi(match[1].str());
+      pulseO.write(1);
+      TOOneshot.attach_us(callback(pulseLow),pulseWidth);
   }
   else{
     printf("Syntax error, check syntax or type: help\n");
